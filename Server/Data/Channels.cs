a868482bcf54e1df537b92a51eeb707f3a6b1886
then <lequanhlol@gmail.com>
2024-11-02 20:20:57 +0700

    Server - update

---
+++ Server/Data/Channels.cs
@@ -0,0 +1,12 @@
+namespace Server.Data
+{
+    public static class Channels
+    {
+        public const string StationConnected = "StationConnected";
+        public const string StationDisconnected = "StationDisconnected";
+        public const string GetOnlineStations = "GetOnlineStations";
+        public const string CamerasConnected = "CamerasConnected";
+        public const string CamerasDisconnected = "CamerasDisconnected";
+        public const string GetOnlineCameras = "GetOnlineCameras";
+    }
+}
