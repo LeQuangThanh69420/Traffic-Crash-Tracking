1e42c99dfe7998cde56a4b4173b2c7ee1ec36f61
then <lequanhlol@gmail.com>
2024-11-08 00:42:56 +0700

    Camera - update Connection

--- Camera/Connection.py
+++ Camera/Connection.py
@@ -1,3 +1,8 @@
+import time
+import base64
+import cv2
+import logging
+from signalrcore.hub_connection_builder import HubConnectionBuilder
 import requests
 
 cameraName = "Camera1"
@@ -24,3 +29,17 @@ def GetCameraToken():
     except requests.exceptions.RequestException as e:
         print(f"Exception: {e}")
         exit()
+
+def PresenceHubConnection():
+    return HubConnectionBuilder()\
+    .with_url(hubUrl + "presence",
+            options={
+                "access_token_factory": GetCameraToken,
+    })\
+    .with_automatic_reconnect({
+        "type": "raw",
+        "keep_alive_interval": 10,
+        "reconnect_interval": 5,
+        "max_attempts": 5
+    })\
+    .build()
