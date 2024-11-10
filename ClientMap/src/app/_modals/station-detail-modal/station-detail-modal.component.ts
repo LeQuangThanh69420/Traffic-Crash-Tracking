192aa9c0621f9090b5d37a5718e71f25b0e81f68
then <lequanhlol@gmail.com>
2024-11-10 22:10:55 +0700

    ClientMap - update

--- ClientMap/src/app/_modals/station-detail-modal/station-detail-modal.component.ts
+++ ClientMap/src/app/_modals/station-detail-modal/station-detail-modal.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 import { PresenceService } from 'src/app/_services/presence.service';
 
 @Component({
@@ -12,16 +11,9 @@ export class StationDetailModalComponent implements OnInit, OnDestroy{
   constructor(
     public presence: PresenceService) { }
 
-  isOpen: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  Open() {
-    this.isOpen.next(true);
-  }
-  Close() {
-    this.isOpen.next(false);
-  }
-
   ngOnInit() {
   }
+
   ngOnDestroy() {
   }
 }
