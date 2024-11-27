f8e9ff5ff09c45dd26de048174a9d93bc9365e41
then <lequanhlol@gmail.com>
2024-11-27 18:57:34 +0700

    Camera - update

--- Camera/streaming.py
+++ Camera/streaming.py
@@ -19,7 +19,7 @@ hub_connection.on("ForcedDisconnect", Connection.Exit)
 classes = LoadData.GetClasses()
 model = YOLO(LoadData.GetModel(1)).to(LoadData.GetDevice())
 
-capture = cv2.VideoCapture("./Camera/videos/cr.mp4")
+capture = cv2.VideoCapture("./Camera/videos/ex1.mp4")
 width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
@@ -65,16 +65,16 @@ while True:
         track_id = track.track_id
         x1, y1, x2, y2 = map(int, track.to_ltrb())
         class_name, (R, G, B) = track.get_det_class()
-
         if track_id in previous_positions:
             previous_tracks[track_id] = previous_positions[track_id]
         else:
             previous_tracks[track_id] = (x1, y1, x2, y2)
         previous_positions[track_id] = (x1, y1, x2, y2)
         current_tracks[track_id] = (x1, y1, x2, y2)
-
+        (start, end, vel) = Draw.Movement(previous_tracks[track_id], (x1, y1, x2, y2))
+        cv2.arrowedLine(frame, start, end, (B, G, R), 2, tipLength=0.2)
         cv2.rectangle(frame, (x1, y1), (x2, y2), (B, G, R), 2)
-        cv2.putText(frame, f'{track_id} {class_name}', (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (B, G, R), 2)
+        cv2.putText(frame, f'{track_id} {class_name} V:{vel:.2f}', (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (B, G, R), 2)
 
     for id1, box1 in current_tracks.items():
         for id2, box2 in current_tracks.items():
